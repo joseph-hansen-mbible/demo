79bf5f5feb353e7ba379e5b609f7ee8132b2ec30
Joseph Hansen <josephclaytonhansen@gmail.com>
2026-03-04 12:15:43 -0600

    boat lantern decals

--- Resources/Components/GameStart/FlickerBoat.cs
+++ Resources/Components/GameStart/FlickerBoat.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Flicker : MonoBehaviour
 {
@@ -6,7 +7,12 @@ public class Flicker : MonoBehaviour
     public Color emissionColor = Color.white;
     public float minIntensity = 1f;
     public float maxIntensity = 5f;
-    
+
+    public float maxDecalIntensity = .2f;
+    public float minDecalIntensity = .05f;
+
+    public DecalProjector[] decalProjectors;
+
     // add noise to the material's emission intensity 
     // material is a URP Lit default with emission enabled
     void Update()
@@ -17,6 +23,16 @@ public class Flicker : MonoBehaviour
             float intensity = Mathf.Lerp(minIntensity, maxIntensity, noise);
             Color finalEmission = emissionColor * intensity;
             mat.SetColor("_EmissionColor", finalEmission);
+        }
+
+        if (decalProjectors != null)
+        {
+            foreach (var decalProjector in decalProjectors)
+            {
+                float noise = Mathf.PerlinNoise(Time.time * 4f, 0f);
+                float opacity = Mathf.Lerp(minDecalIntensity, maxDecalIntensity, noise);
+                decalProjector.fadeFactor = opacity;
+            }
         }
     }
 }
